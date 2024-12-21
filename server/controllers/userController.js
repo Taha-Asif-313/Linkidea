ddb665d1984c398c22feb20d2d577ae9bad1653d
Tahaiff <tahaasifaqwe@gmail.com>
2024-12-21 11:12:39 +0500

    Updated

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -112,7 +112,7 @@ export const login = async (req, res) => {
     return res.status(400).json({
       success: false,
       message: "internal server error !",
-      error: error,
+      error: error.message,
     });
   }
 };
