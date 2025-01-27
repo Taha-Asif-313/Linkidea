f89e8c9712f602c9e914124a42ecb5f3c0c3c00c
Tahaiff <tahaasifaqwe@gmail.com>
2025-01-27 21:55:57 +0500

    Updated

--- server/storage/multerconfig.js
+++ server/storage/multerconfig.js
@@ -1,5 +1,4 @@
 import  multer  from "multer";
-import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -11,11 +10,6 @@ const __dirname = path.dirname(__filename);
 
 // Define the upload directory
 const uploadDir = path.join(__dirname, '../uploads');
-
-// Create the directory if it doesn't exist
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true });
-}
 
 // Set up multer storage
 const storage = multer.diskStorage({
