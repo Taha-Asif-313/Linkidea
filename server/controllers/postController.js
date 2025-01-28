8bd8817082f7e89020c8abe6606005284745f0e1
Tahaiff <tahaasifaqwe@gmail.com>
2025-01-28 14:57:52 +0500

    Updated

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -26,7 +26,9 @@ export const createpost = async (req, res) => {
     const { title, content } = req.body; // Assuming file is Base64-encoded
     const userId = req.user.id;
     const thumbnail = req.file; // File information from multer
-    console.log(thumbnail);
+ if(thumbnail){
+  console.log(thumbnail);
+ }
 
 
     // Validate required fields
@@ -61,7 +63,7 @@ export const createpost = async (req, res) => {
       likes:0,
       views:0,
       comments:[],
-      thumnail: `https://linkidea-server.vercel.app/uploads/${thumbnail.originalname}`, // Save the filename
+      thumnail: `https://linkidea-server.vercel.app/uploads/${thumbnail?.thumbnail.originalname}`, // Save the filename
       user: user._id,
       username: user.username,
     });
